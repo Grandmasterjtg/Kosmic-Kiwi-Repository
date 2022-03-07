9801c129fdc5df37b44d69e8ffb4f626d654d680
Brock Blanck <blanckbrock838@gmail.com>
2022-03-07 15:25:29 -0600

    test Items and Slots

---
+++ KosmicKiwi/TempScript/Item.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+export var m_name : String
+export var m_stack_size : int
+export var m_id : int
+export var m_droppable : bool
+export var m_sprite : Texture
---
+++ KosmicKiwi/TempScript/Slot.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var test = preload("res://TempScene/Item.tscn").instance()
+onready var sprite = $Panel/Sprite
+var display = true
+
+func _ready():
+#	if test:
+#		get_node("Panel/Sprite").texture = test.m_sprite
+	pass
+		
+func _process(delta):
+	if Input.is_action_just_pressed("move_down") and test:
+		if display:
+			sprite.texture = test.m_sprite
+			display = !display
+		else:
+			sprite.texture = null
+			display = !display
