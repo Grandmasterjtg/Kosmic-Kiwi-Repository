12152940d5b3f4795731b9edf0b4d53ab7522edf
hooverm7306 <hooverm7306@my.uwstout.edu>
2022-03-09 14:36:20 -0600

    Improved Player Movement
    updated the player movement script to be more efficient

--- KosmicKiwi/Scripts/PlayerMovement.gd
+++ KosmicKiwi/Scripts/PlayerMovement.gd
@@ -9,40 +9,21 @@ const ACTION_LEFT := "move_left"
 const ACTION_RIGHT := "move_right"
 const ACTION_UP := "move_up"
 
-export var speed := 100.0
-var direction := "S"
+export var m_speed := 100.0
+var m_direction := Vector2(0, 1)
 
-func _physics_process(delta):
-	var horizontal_movement = speed * delta * (Input.get_action_strength(ACTION_RIGHT) - Input.get_action_strength(ACTION_LEFT))
-	var vertical_movement = speed * delta * (Input.get_action_strength(ACTION_DOWN) - Input.get_action_strength(ACTION_UP))
+func _physics_process(delta) -> void:
+	var horizontal_movement = m_speed * delta * \
+		(Input.get_action_strength(ACTION_RIGHT) - Input.get_action_strength(ACTION_LEFT))
+	var vertical_movement = m_speed * delta * \
+		(Input.get_action_strength(ACTION_DOWN) - Input.get_action_strength(ACTION_UP))
 	move_local_x(horizontal_movement)
 	move_local_y(vertical_movement)
-	if horizontal_movement == 0 and vertical_movement < 0:
-		direction = "N"
-		get_child(0).rotation_degrees = 180
-	elif horizontal_movement > 0 and vertical_movement < 0:
-		direction = "NE"
-		get_child(0).rotation_degrees = 225
-	elif horizontal_movement > 0 and vertical_movement == 0:
-		direction = "E"
-		get_child(0).rotation_degrees = 270
-	elif horizontal_movement > 0 and vertical_movement > 0:
-		direction = "SE"
-		get_child(0).rotation_degrees = 315
-	elif horizontal_movement == 0 and vertical_movement > 0:
-		direction = "S"
-		get_child(0).rotation_degrees = 0
-	elif horizontal_movement < 0 and vertical_movement > 0:
-		direction = "SW"
-		get_child(0).rotation_degrees = 45
-	elif horizontal_movement < 0 and vertical_movement == 0:
-		direction = "W"
-		get_child(0).rotation_degrees = 90
-	elif horizontal_movement < 0 and vertical_movement < 0:
-		direction = "NW"
-		get_child(0).rotation_degrees = 135
+	if horizontal_movement != 0 or vertical_movement != 0:
+		m_direction = Vector2(horizontal_movement, vertical_movement).normalized()
+	get_child(0).rotation = get_angle_to(position + m_direction) - PI / 2
 
-func _input(event):
+func _input(event) -> void:
 	if event.is_action_pressed(ACTION_HOTBAR_1):
 		instantiate_image(0.1)
 	elif event.is_action_pressed(ACTION_HOTBAR_2):
@@ -52,32 +33,14 @@ func _input(event):
 	elif event.is_action_pressed(ACTION_HOTBAR_4):
 		instantiate_image(1)
 
-func instantiate_image(scale):
+func instantiate_image(scale) -> void:
 	var sprite = Sprite.new()
-	sprite.texture = self.get_child(0).texture
-	sprite.position = self.position
+	sprite.texture = get_child(0).texture
+	sprite.position = position
 	sprite.scale *= scale
 	sprite.z_index = 0
-	var size_x = sprite.scale.x * sprite.texture.get_size().x / 2 + self.get_child(0).texture.get_size().x / 2
-	var size_y = sprite.scale.y * sprite.texture.get_size().y / 2 + self.get_child(0).texture.get_size().y / 2
-	if direction == "N":
-		sprite.position.y -= size_y + 10
-	if direction == "NE":
-		sprite.position.x += size_x
-		sprite.position.y -= size_y
-	if direction == "E":
-		sprite.position.x += size_x + 10
-	if direction == "SE":
-		sprite.position.x += size_x
-		sprite.position.y += size_y
-	if direction == "S":
-		sprite.position.y += size_y + 10
-	if direction == "SW":
-		sprite.position.x -= size_x
-		sprite.position.y += size_y
-	if direction == "W":
-		sprite.position.x -= size_x + 10
-	if direction == "NW":
-		sprite.position.x -= size_x
-		sprite.position.y -= size_y
+	var size_x = sprite.scale.x * sprite.texture.get_size().x / 2 + get_child(0).texture.get_size().x / 2 + 10
+	var size_y = sprite.scale.y * sprite.texture.get_size().y / 2 + get_child(0).texture.get_size().y / 2 + 10
+	sprite.position.x += m_direction.x * size_x
+	sprite.position.y += m_direction.y * size_y
 	get_parent().add_child(sprite)
