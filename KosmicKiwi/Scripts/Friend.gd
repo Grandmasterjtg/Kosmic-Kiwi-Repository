3c714523eb3385ce1359b9c76f403f4fd20ecb56
John Branton <brantonj2795@my.uwstout.edu>
2022-03-11 15:25:08 -0600

    Finished Friend Follow script

--- KosmicKiwi/Scripts/Friend.gd
+++ KosmicKiwi/Scripts/Friend.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
@@ -6,11 +6,26 @@ extends Node2D
 # var b = "text"
 
 
-export var m_speed := 100.0
-var m_direction := Vector2(0, 1)
+# export var m_speed := 100.0
+const PLAYER_PATH := "Player"
 
-func _physics_process(delta) -> void:
-	var horizontal_movement = m_speed * delta
-	var vertical_movement = m_speed * delta
-	move_local_x(horizontal_movement)
-	move_local_y(vertical_movement)
+export var m_follow_distance := 200
+export var m_stop_distance := 100
+var should_move := false
+var m_target
+
+func _ready():
+	m_target = get_parent().get_node(PLAYER_PATH)
+
+func _physics_process(delta):
+	if (m_target != null):
+		var target_direction = m_target.global_position - global_position
+		
+		if (target_direction.length_squared() > (m_follow_distance * m_follow_distance)):
+			should_move = true
+		elif (target_direction.length_squared() < (m_stop_distance * m_stop_distance)):
+			should_move = false
+			
+		if (should_move):
+			look_at(m_target.position)
+			move_and_slide(target_direction, Vector2.UP)
