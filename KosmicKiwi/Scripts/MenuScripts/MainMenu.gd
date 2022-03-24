0f3b8d218b7707eaa604cca0c2945b21670b1ff5
johnnybboy <brantonj2795@my.uwstout.edu>
2022-03-24 18:43:09 -0500

    Menus Created

---
+++ KosmicKiwi/Scripts/MenuScripts/MainMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var m_close_button
+var m_start_button
+export var first_level_path = ""
+
+func _ready():
+	m_close_button = $CloseButton
+	m_close_button.connect("pressed",self,"_on_CloseButton_pressed")
+	
+	m_start_button = $StartButton
+	m_close_button.connect("pressed",self,"_on_StartButton_pressed")
+
+func _on_CloseButton_pressed():
+	get_tree().quit()
+
+func _on_StartButton_pressed():
+	get_tree().change_scene(first_level_path)
