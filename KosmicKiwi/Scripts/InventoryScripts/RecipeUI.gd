218cc5e7d9c1a0258c666d02f4313fc922145599
Brock Blanck <blanckbrock838@gmail.com>
2022-03-24 22:20:29 -0500

    Recipe changes

--- KosmicKiwi/Scripts/InventoryScripts/RecipeUI.gd
+++ KosmicKiwi/Scripts/InventoryScripts/RecipeUI.gd
@@ -4,3 +4,6 @@ const SLOT_CLASS = preload("res://Scenes/UI/Inventory/Slot.tscn")
 
 onready var m_item_to_craft = $ItemToCraft
 onready var m_recipe_display = $Recipes
+
+func initialize_recipe(recipe: Recipe):
+	m_item_to_craft.initialize_item(recipe.get_item_name(), recipe.get_quantity())
