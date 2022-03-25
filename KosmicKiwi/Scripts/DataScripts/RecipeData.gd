9fec9cca9300702cd647cec5cb4790e9719bb662
Brock Blanck <blanckbrock838@gmail.com>
2022-03-24 19:28:50 -0500

    Recipe's
    I created the Recipe data structure

--- KosmicKiwi/Scripts/DataScripts/RecipeData.gd
+++ KosmicKiwi/Scripts/DataScripts/RecipeData.gd
@@ -5,3 +5,17 @@ var m_recipe_data : Dictionary
 
 func _ready() -> void:
 	m_data = load_data(FILE_PATH)
+	
+	# for each to be crafted int eh json file
+	for item in m_data:
+		# for each recipe for the item to be crafted
+		for i in range(m_data[item].size()):
+			# create a Recipe object using the data from the recipe
+			m_data[item][i] = json_to_recipe(m_data[item][i])
+			# set the Recipe's name to the item to be crated's name
+			m_data[item][i].set_item_name(item)
+
+# converts a json dictionary to a Recipe object
+# returns the Recipe ojbect created	
+func json_to_recipe(json_dct: Dictionary) -> Recipe:
+	return Recipe.new(json_dct["Recipe"], json_dct["Quantity"])
