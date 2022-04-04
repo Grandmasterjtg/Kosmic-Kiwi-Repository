d8febd5f030dddb382e2449e134cda9ded9c7630
John Branton <brantonj2795@my.uwstout.edu>
2022-04-04 15:26:13 -0500

    Puzzle script worked on

--- KosmicKiwi/Scripts/Puzzle Scripts/Puzzle.gd
+++ KosmicKiwi/Scripts/Puzzle Scripts/Puzzle.gd
@@ -1,13 +1,29 @@
 extends Node
 
-class_name Puzzle
-
 var m_id : int
 var m_solved := false
 
 func _ready():
+	PuzzleManager.add_puzzle(m_id)
+	if PuzzleManager.check_puzzle_solved(m_id):
+		solve_puzzle()
+
+func solve_puzzle():
+	print("Solved!")
+	# play the solving animation
+	$AnimatedSprite.play("left")
+	
+	if PuzzleManager.check_puzzle_solved(m_id):
+		# play the last frame of the solving animation
+		$AnimatedSprite.set_frame($AnimatedSprite.get_sprite_frames().get_frame_count("left")-1)
+	
+	PuzzleManager.set_puzzle_solved(m_id, true)
+
+func ready_puzzle():
 	pass
 
-func update_state():
-	pass
-
+func _process(delta):
+	if Input.is_action_just_pressed("hotbar_1"):
+		solve_puzzle()
+	if Input.is_action_just_pressed("hotbar_2"):
+		solve_puzzle()
--- KosmicKiwi/Scripts/Puzzle Scripts/PuzzleManager.gd
+++ KosmicKiwi/Scripts/Puzzle Scripts/PuzzleManager.gd
@@ -2,17 +2,20 @@ extends Node
 
 var m_puzzles : Dictionary
 
+signal test
+
 func _ready():
-	# gather all of the puzzles?
-	# set ids?
-	pass
+	emit_signal("test")
 
 func add_puzzle(puzzle_id: int):
-	m_puzzles[puzzle_id] = false
-	pass
+	if !m_puzzles.has(puzzle_id):
+		m_puzzles[puzzle_id] = false
+	else:
+		printerr("Puzzle attempted to add twice:")
+		printerr(puzzle_id)
 
 func check_puzzle_solved(puzzle_id: int) -> bool:
-	if (m_puzzles.has(puzzle_id)):
+	if m_puzzles.has(puzzle_id):
 		return m_puzzles[puzzle_id]
 	else:
 		printerr("No puzzle was found for puzzle_id:")
@@ -20,7 +23,7 @@ func check_puzzle_solved(puzzle_id: int) -> bool:
 		return false
 
 func set_puzzle_solved(puzzle_id: int, state: bool):
-	if (m_puzzles.has(puzzle_id)):
+	if m_puzzles.has(puzzle_id):
 		m_puzzles[puzzle_id] = state
 	else:
 		printerr("No puzzle was found for puzzle_id:")
