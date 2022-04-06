5d173abc639e637743b0b582fad26994430101bb
johnnybboy <brantonj2795@my.uwstout.edu>
2022-04-06 11:55:47 -0500

    Updated Puzzle scene

--- KosmicKiwi/Scripts/Puzzle Scripts/Puzzle.gd
+++ KosmicKiwi/Scripts/Puzzle Scripts/Puzzle.gd
@@ -6,6 +6,15 @@ var m_id : int
 var m_solved := false
 
 func _ready():
+	# setup puzzle
+	ready_puzzle()
+	
+	# connect to interactable
+	$Interactable.connect("interacted",self,"on_interact")
+
+func ready_puzzle():
+	m_id = self.get_index()
+	print("index: " + str(m_id))
 	PuzzleManager.add_puzzle(m_id)
 	if PuzzleManager.check_puzzle_solved(m_id):
 		solve_puzzle()
@@ -13,16 +22,17 @@ func _ready():
 func solve_puzzle():
 	print("Solved!")
 	# play the solving animation
-	$AnimatedSprite.play("left")
+#	$AnimatedSprite.play("solved")
 	
 	# if the puzzle is already solved, play only the last frame of the animation
-	if PuzzleManager.check_puzzle_solved(m_id):
-		$AnimatedSprite.set_frame($AnimatedSprite.get_sprite_frames().get_frame_count("left")-1)
-	else: 
-		PuzzleManager.set_puzzle_solved(m_id, true)
+#	if PuzzleManager.check_puzzle_solved(m_id):
+#		$AnimatedSprite.set_frame($AnimatedSprite.get_sprite_frames().get_frame_count("solved")-1)
+#	else: 
+#		PuzzleManager.set_puzzle_solved(m_id, true)
 
-func ready_puzzle():
-	pass
+
+func on_interact():
+	solve_puzzle()
 
 func _process(delta):
 	if Input.is_action_just_pressed("hotbar_1"):
