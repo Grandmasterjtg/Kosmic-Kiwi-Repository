aebd51d2a7d866c9f4288fc0d804e09bd977083a
johnnybboy <brantonj2795@my.uwstout.edu>
2022-04-07 22:57:32 -0500

    Started Level_1

--- KosmicKiwi/Scripts/PlanetScripts/LevelManager.gd
+++ KosmicKiwi/Scripts/PlanetScripts/LevelManager.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-export var m_ship_node_path := ""
-export var m_nav_menu_path := ""
 export var m_planet_index := 0
 
 func _ready() -> void:
@@ -9,8 +7,8 @@ func _ready() -> void:
 	setup_ship()
 
 func setup_ship():
-	var ship = get_node(m_ship_node_path)
-	var nav_menu = get_node(m_nav_menu_path)
+	var ship = $Actors/Ship
+	var nav_menu = $UI_Layer/NavMenu
 	if ship and nav_menu:
 		ship.connect("interacted", nav_menu, "toggle_nav_menu")
 	else:
