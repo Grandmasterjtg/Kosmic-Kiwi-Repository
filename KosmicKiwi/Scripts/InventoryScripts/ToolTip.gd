a69e6c25b54919dd88a4bd22d0889cebe1acacf3
Brock Blanck <blanckbrock838@gmail.com>
2022-04-17 15:15:30 -0500

    Auto Wrap ToolTip test

--- KosmicKiwi/Scripts/InventoryScripts/ToolTip.gd
+++ KosmicKiwi/Scripts/InventoryScripts/ToolTip.gd
@@ -1,7 +1,12 @@
-extends Label
+extends PanelContainer
 
 
 func initialize_tip(item_name: String):
 	var text = ItemData.get_tool_tip(item_name)
 	if text != null:
-		self.text = text
+		$ToolTip.text = text
+	
+	print($ToolTip.get_total_character_count())
+	if $ToolTip.get_total_character_count() > 15:
+		$ToolTip.autowrap = true
+		rect_size.x = 280
