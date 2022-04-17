9478e643e3c19a3224ee30ed2788c42eeb2a5451
Brock Blanck <blanckbrock838@gmail.com>
2022-04-17 17:31:17 -0500

    Finished label Layout and One time deposits

--- KosmicKiwi/Scripts/InventoryScripts/ToolTip.gd
+++ KosmicKiwi/Scripts/InventoryScripts/ToolTip.gd
@@ -1,12 +1,35 @@
 extends PanelContainer
 
-
+# takes the name of an item
+# sets up the tool tip display with the proper tool tip
 func initialize_tip(item_name: String):
-	var text = ItemData.get_tool_tip(item_name)
-	if text != null:
+	var tool_tip = ItemData.get_tool_tip(item_name)
+	var placeable = ItemData.is_placeable(item_name)
+	var on_hotbar = InventoryData.get_is_interactable(ItemData.get_category(item_name))
+	
+	# null check for item validation
+	if tool_tip != null and placeable != null:
+		# add the name of the time to the display
+		var text = "Name: " + item_name
+		
+		# add the tool tip to the display
+		text += "\n\n" + tool_tip
+		
+		# add whether or not the item can be put onto the hotbar
+		if on_hotbar:
+			text += "\n\nHotbar Item: Yes"
+		else:
+			text += "\n\nHotbar Item: No"
+		
+		# add whether or not the item is placeable
+		if placeable:
+			text += "\n\nPlaceable: Yes"
+		else:
+			text += "\n\nPlaceable: No"
+			
 		$ToolTip.text = text
 	
-	print($ToolTip.get_total_character_count())
-	if $ToolTip.get_total_character_count() > 15:
-		$ToolTip.autowrap = true
-		rect_size.x = 280
+		if tool_tip.length() > 15:
+			print("ToolTip: Big")
+			$ToolTip.autowrap = true
+			rect_size.x = 260
