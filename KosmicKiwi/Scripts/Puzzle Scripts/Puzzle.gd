fb8aaab03bd28298727734eb8d211360c54fe267
hooverm7306 <hooverm7306@my.uwstout.edu>
2022-04-20 14:43:07 -0500

    some new props

--- KosmicKiwi/Scripts/Puzzle Scripts/Puzzle.gd
+++ KosmicKiwi/Scripts/Puzzle Scripts/Puzzle.gd
@@ -2,14 +2,22 @@ extends Node2D
 
 class_name Puzzle
 
+const FILE_PATH = "res://ArtAssets/SpriteFrames/"
+const FILE_TYPE = ".tres"
+const DEFAULT_ANIM = "default"
+const SOLVED_ANIM = "solved_"
+
 var m_id : Vector2
-export var type : String
+var m_planet : String
+onready var m_animator = $AnimatedSprite
+onready var m_collider = $Collider
 
 func _ready():
 	# setup puzzle
 	ready_puzzle()
 
 func ready_puzzle():
+	m_planet = PlanetManager.get_active_planet().get_planet_name()
 	m_id = self.get_global_position()
 	PuzzleManager.add_puzzle(m_id)
 	if PuzzleManager.check_puzzle_solved(m_id):
@@ -19,12 +27,12 @@ func solve_puzzle():
 	var already_solved = PuzzleManager.check_puzzle_solved(m_id)
 	
 	# play the solving animation
-	$AnimatedSprite.play("solved")
-	$Collider.disabled = true
+	m_animator.play(SOLVED_ANIM + m_planet)
+	m_collider.disabled = true
 	
 	if already_solved:
 		# if the puzzle is already solved, play only the last frame of the animation
-		$AnimatedSprite.set_frame($AnimatedSprite.get_sprite_frames().get_frame_count("solved")-1)
+		m_animator.set_frame(m_animator.get_sprite_frames().get_frame_count(SOLVED_ANIM + m_planet)-1)
 	else:
 		# set the puzzle solved
 		PuzzleManager.set_puzzle_solved(m_id, true)
