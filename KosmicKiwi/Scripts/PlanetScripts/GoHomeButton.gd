94f6bf455a2b8d08a8468fb0c5edcfdcbed3950d
Brock Blanck <blanckbrock838@gmail.com>
2022-04-29 15:55:49 -0500

    Added New Zealand to planet manager

--- KosmicKiwi/Scripts/PlanetScripts/GoHomeButton.gd
+++ KosmicKiwi/Scripts/PlanetScripts/GoHomeButton.gd
@@ -2,6 +2,7 @@ extends TextureButton
 
 
 const SCENE_TO_LOAD := "res://Levels/EndScreen.tscn"
+const PLANET_INDEX := 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,5 @@ func _ready():
 
 
 func on_button_down():
+	PlanetManager.set_active_planet(PlanetManager.get_planet_at_index(PLANET_INDEX))
 	LoadingManager.load_new_scene(SCENE_TO_LOAD)
